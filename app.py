be4c2cd3a293b38b8d9061c27ba4352622f1078a
Josh Garrett <jjgarrett0@gmail.com>
2017-12-14 10:29:18 -0800

    INitial updates, still need to finish with conversion to my data model.

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ from schema import schema
 app = Flask(__name__)
 app.debug = True
 
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True,
+                                                           context={'session': db_session}))
 
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True, context={'session': db_session}))
 
 @app.route('/')
 def index():
-	return "Go to /graphql"
+    return "Go to /graphql"
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
